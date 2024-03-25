a1a4f8d3fc6ace4d705866089f70af2fcb7f6fbe
jodyfordmsft <jodyford@microsoft.com>
2024-03-25 11:22:00 -0400

    added workflow

---
+++ sql_scripts/create_and_query_sample_data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE SampleDataset (  
+    id INT PRIMARY KEY,  
+    first_name NVARCHAR(50),  
+    last_name NVARCHAR(50),  
+    email NVARCHAR(100),  
+    city NVARCHAR(50),  
+    country NVARCHAR(50)  
+);  
